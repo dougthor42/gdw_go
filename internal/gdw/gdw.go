bea199ff9b15c6b8cb13784b651e12f993df3e49
Douglas Thor <doug.thor@gmail.com>
2021-04-02 16:44:18 -0700

    Add MaxDistSqrd

--- internal/gdw/gdw.go
+++ internal/gdw/gdw.go
@@ -12,7 +12,11 @@ var FlatLengths = map[float64]float64{
 	75:  22.22,
 	100: 32.5,
 	125: 42.5,
-	150: 75.5,
+	150: 57.5,
+}
+
+type Coord struct {
+	X, Y float64
 }
 
 //export Hello
@@ -28,18 +32,33 @@ func Hello() string {
 // func MaxDistSqrd() {}
 
 // Return the flat's Y location WRT the wafer center for a given diameter.
-func FlatLocation(diameter float64) float64 {
-	// We must cast diameter to a float64 to do the math we want.
-	var dia float64 = float64(diameter)
+func FlatLocation(dia float64) float64 {
 	// Default case: the flat is at the wafer edge.
 	var flatY float64
-	flatY = -dia / 2
+	flatY = -dia / 2.0
 
 	// If the flat length is defined by SEMI-M1-0302, we can use that to
 	// calculate the Y location.
-	if val, ok := FlatLengths[diameter]; ok {
+	if val, ok := FlatLengths[dia]; ok {
 		flatY = -math.Sqrt(math.Pow(dia/2, 2) - math.Pow(val*0.5, 2))
 	}
 	return flatY
 
+}
+
+func MaxDistSqrd(center, size Coord) float64 {
+	halfX := size.X / 2.0
+	halfY := size.Y / 2.0
+
+	if center.X < 0 {
+		halfX = -halfX
+	}
+
+	if center.Y < 0 {
+		halfY = -halfY
+	}
+
+	dist := math.Pow(center.X+halfX, 2) + math.Pow(center.Y+halfY, 2)
+
+	return dist
 }
