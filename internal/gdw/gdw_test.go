d5c1b1a3e229c940b8fda644279aff3d7b633060
Douglas Thor <doug.thor@gmail.com>
2021-04-02 16:44:30 -0700

    Add test

--- internal/gdw/gdw_test.go
+++ internal/gdw/gdw_test.go
@@ -2,8 +2,13 @@ package gdw
 
 import (
 	"fmt"
+	"math"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
+
+const tolerance = 0.00001
 
 func TestHello(t *testing.T) {
 	want := "Hello World"
@@ -17,7 +22,7 @@ func TestFlatLocation(t *testing.T) {
 		diameter float64
 		want     float64
 	}{
-		{50, -23.70562},
+		{50, -23.7056196},
 		{75, -35.8164473},
 		{100, -47.2857008},
 		{150, -69.2707550},
@@ -26,12 +31,69 @@ func TestFlatLocation(t *testing.T) {
 		{237.68, -118.84},
 	}
 
+	// Taken from the docs https://pkg.go.dev/github.com/google/go-cmp/cmp#Equal
+	// with help from https://dev.to/juliaferraioli/testing-in-go-testing-floating-point-numbers-4i0a
+	// TODO: How to make this DRY?
+	opt := cmp.Comparer(func(x, y float64) bool {
+		delta := math.Abs(x - y)
+		mean := math.Abs(x+y) / 2.0
+		if math.IsNaN(delta / mean) {
+			return true
+		}
+		return (delta / mean) < tolerance
+	})
+
 	for _, tt := range tables {
 		testName := fmt.Sprintf("%f", tt.diameter)
 		t.Run(testName, func(t *testing.T) {
 			got := FlatLocation(tt.diameter)
-			if got != tt.want {
+			if !cmp.Equal(got, tt.want, opt) {
 				t.Errorf("Got %f, wanted %f", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMaxDistSqrd(t *testing.T) {
+	tables := []struct {
+		center Coord
+		size   Coord
+		want   float64
+	}{
+		{Coord{0, 0}, Coord{2, 2}, 2},
+		{Coord{0, 0}, Coord{6, 8}, 25},
+		{Coord{0, 0}, Coord{2, 36}, 325},
+		{Coord{0, 0}, Coord{0, 0}, 0},
+		{Coord{0.5, 0.5}, Coord{1, 1}, 2},
+		{Coord{0, 0}, Coord{3.14, 2.718}, 4.311781},
+		{Coord{0, -10}, Coord{3.14, 2.718}, 131.491781},
+		{Coord{-10, 0}, Coord{3.14, 2.718}, 135.711781},
+		{Coord{-10, -10}, Coord{3.14, 2.718}, 262.891781},
+		{Coord{0, 10}, Coord{3.14, 2.718}, 131.491781},
+		{Coord{10, 0}, Coord{3.14, 2.718}, 135.711781},
+		{Coord{10, 10}, Coord{3.14, 2.718}, 262.891781},
+		{Coord{100000, 100000}, Coord{2, 2}, 20000400002},
+		{Coord{1000, 0}, Coord{100, 0.00001}, 1102500},
+	}
+
+	// Taken from the docs https://pkg.go.dev/github.com/google/go-cmp/cmp#Equal
+	// with help from https://dev.to/juliaferraioli/testing-in-go-testing-floating-point-numbers-4i0a
+	// TODO: How to make this DRY?
+	opt := cmp.Comparer(func(x, y float64) bool {
+		delta := math.Abs(x - y)
+		mean := math.Abs(x+y) / 2.0
+		if math.IsNaN(delta / mean) {
+			return true
+		}
+		return (delta / mean) < tolerance
+	})
+
+	for _, tt := range tables {
+		testName := "foo"
+		t.Run(testName, func(t *testing.T) {
+			got := MaxDistSqrd(tt.center, tt.size)
+			if !cmp.Equal(got, tt.want, opt) {
+				t.Errorf("Got %f, wanted %f.", got, tt.want)
 			}
 		})
 	}
